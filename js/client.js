f40c8296ca6a02994a0ba38b6d36dd4775088bba
Ty Tia <TyTia77@gmail.com>
2017-09-27 16:43:04 +1000

    added

---
+++ js/client.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Router, Route, IndexRoute, hashHistory } from "react-router";
+
+import Todos from "./pages/Todos";
+import Layout from "./pages/Layout";
+
+const app = document.getElementById('app');
+
+ReactDOM.render(
+  <Router history={hashHistory}>
+    <Route path="/" component={Layout}>
+      <IndexRoute component={Todos}></IndexRoute>
+      <Route path="*" component={Todos} />
+    </Route>
+  </Router>,
+app);
